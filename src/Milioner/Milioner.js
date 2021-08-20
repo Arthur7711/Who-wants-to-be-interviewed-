7ab82c8ec6cbe20b987c8321ba9f4fe394e822c1
Arthur Ghazaryan <newartroll11@gmail.com>
2021-08-20 14:24:03 +0400

    feat:(add useffect & fix the bug)

--- src/Milioner/Milioner.js
+++ src/Milioner/Milioner.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Questions from "./components/questions/Questions";
 import Answers from "./components/answers/Answers";
 import AnswersQuestions from "./anssersQuestions/AnssersQuestions";
@@ -16,14 +16,18 @@ export default function Milioner() {
       console.log(true);
       setQuestionNum((prevNum) => prevNum + 1);
       setPoint((prevPoint) => prevPoint + 1);
-      setData(AnswersQuestions[questionNum]);
+
       console.log(questionNum);
     } else {
       setPoint(0);
-      setQuestionNum(0)
+      setQuestionNum(0);
       console.log("you lose this game");
     }
   };
+  
+  useEffect(() => {
+    setData(AnswersQuestions[questionNum]);
+  }, [questionNum]);
 
   return (
     <div className={styles.parentMilioner}>
